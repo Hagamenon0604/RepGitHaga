11e9965735e610082bdd98d066bf1a6b158f0d8a
hagamenoncortez <hagamenoncortez@hotmail.com>
2021-05-11 21:35:07 -0300

    Make functional_tests an app using LiveServerTestCase.

--- functional_tests.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-
-if __name__ == '__main__':
-    browser = webdriver.Chrome("chromedriver")
-    browser.get("http://localhost:8000")
-
-assert 'worked successfully' in browser.title
